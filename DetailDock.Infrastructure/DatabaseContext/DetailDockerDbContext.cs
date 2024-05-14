c348e9eb356af1c828e9c90449cb60ea7848896d
Bhavesh Shrimali <shrimalibhavesh4@gmail.com>
2024-05-14 21:15:54 +0530

    CosmosDB and DbContext Setup

---
+++ DetailDock.Infrastructure/DatabaseContext/DetailDockerDbContext.cs
@@ -0,0 +1,40 @@
+﻿using DetailDock.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetailDock.Infrastructure.DatabaseContext
+{
+    public class DetailDockerDbContext:DbContext
+    {
+        public DetailDockerDbContext(DbContextOptions<DetailDockerDbContext> options):base(options)
+        {
+                
+        }
+        public DbSet<Program> Programs { get; set; }
+        public DbSet<BasicInfo> BasicInfo { get; set; }
+        public DbSet<QuestionType> QuestionTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Program>()
+                    .ToContainer("Program")
+                    .HasPartitionKey(e => e.Id);
+
+
+            modelBuilder.Entity<BasicInfo>()
+                .ToContainer("PersonalInformation")
+                .HasPartitionKey(c => c.Id);
+
+
+            modelBuilder.Entity<QuestionType>()
+                .ToContainer("QuestionType")
+                .HasPartitionKey(c => c.Id);
+        }
+
+    }
+}
--- DetailDock/Program.cs
+++ DetailDock/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton((serviceProvider) =>
+{
+    var cosmosClient = new CosmosClient("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+    var database = cosmosClient.GetDatabase("FormData");
+    var container = database.GetContainer("QuestionType");
+    return container;
+});
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
